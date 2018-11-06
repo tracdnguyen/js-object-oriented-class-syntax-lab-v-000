a200fa3c4fe2edad5b0c8840a216186a2fcdeb6a
Trac Nguyen <nguyendtrac@gmail.com>
2018-11-06 01:59:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,3 +25,9 @@ class BoardMember {
     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
   }
 }
+
+class Ceo extends User {
+  hireEmployee() {
+    return "Welcome aboard!"
+  }
+}
