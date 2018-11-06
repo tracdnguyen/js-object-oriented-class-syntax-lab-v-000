a2d071283f832e17d1f5ef8d86a49285f04cf797
Trac Nguyen <nguyendtrac@gmail.com>
2018-11-06 01:57:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-class boardMembers {
+class BoardMember {
   constructor(name, homeState, training) {
     this.name = name;
     this.homeState = homeState;
     this.training = training;
   }
+
+  veto() {
+    return "No, I must disagree"
+  }
+  
 }
