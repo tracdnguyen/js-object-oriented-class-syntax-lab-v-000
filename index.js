3e38c50838dcd52abbadec06cb583084217bd871
Trac Nguyen <nguyendtrac@gmail.com>
2018-11-06 01:59:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,5 +21,7 @@ class BoardMember {
     return "You will see great things from Scuber."
   }
 
-  
+  sayHi() {
+    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+  }
 }
